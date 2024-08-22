030f6819030b5fcf991a6211600b79624a317648
GuriZenit <gurizenit@gmail.com>
2024-08-22 04:22:27 -0300

    style: Update CSS for improved layout and responsiveness

--- script.js
+++ script.js
@@ -1,28 +1,41 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const gif = document.querySelector(".middle-gif");
-  const playButton = document.getElementById("play-button");
-  const audio = document.getElementById("bgm");
-  const bgmInfo = document.querySelectorAll(".bgm-info");
+  const mobileBackground = document.querySelector(".mobile-background");
   const stopButton = document.getElementById("bgm-stop-button");
+  const playButton = document.getElementById("play-button");
+  const bgmInfo = document.querySelectorAll(".bgm-info");
+  const bgmTitle = document.getElementById("bgm-title");
+  const gif = document.querySelector(".middle-gif");
+  const body = document.querySelector("body");
+  const audio = document.getElementById("bgm");
+  
+  const songNames = ["なつかしいうた", "とりはそと、おにはうち", "The End"]
+  const randomTheme = Math.floor(Math.random() * songNames.length);
 
+  mobileBackground.src = `assets/img/background-${randomTheme}.png`;
+  bgmTitle.innerText = `Song: ${songNames[randomTheme]}`;
+  audio.lastElementChild.src = `assets/audio/song-${randomTheme}.ogg`;
+  body.style.backgroundImage = `url(assets/img/background-${randomTheme}.png)`;
+  
+  audio.load();
+  
   playButton.addEventListener("click", async function () {
     audio.play().catch(function (error) {
       console.log("Autoplay was prevented:", error);
     });
-
+    
     playButton.style.display = "none";
     gif.style.display = "block";
     stopButton.style.display = "block";
     bgmInfo.forEach(function (info) {
       info.style.display = "block";
     });
-
+    
     gif.animate([{ opacity: 0 }, { opacity: 1 }], {
       duration: 1000,
       fill: "forwards",
     });
   });
-
+  
 
   const audioEnded = function () {
     audio.currentTime = 0;
